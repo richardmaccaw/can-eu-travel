d492236d8099fe72e3e8e620d6597239cf20aa7a
richardmaccaw <ricci6@gmail.com>
2025-06-06 21:43:28 +0100

    style: adjust scrollbar width for vertical and horizontal orientations

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -51,9 +51,9 @@ function ScrollBar({
       className={cn(
         "flex touch-none p-px transition-colors select-none",
         orientation === "vertical" &&
-          "h-full w-1 border-l border-l-transparent",
+        "h-full w-2 border-l border-l-transparent",
         orientation === "horizontal" &&
-          "h-1 flex-col border-t border-t-transparent",
+        "h-2 flex-col border-t border-t-transparent",
         className
       )}
       {...props}
