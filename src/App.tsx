660255ceec1e33124e3755e81ff9177f8a27ec02
richardmaccaw <ricci6@gmail.com>
2025-06-06 23:07:47 +0100

    Update App component layout and spacing
    - Adjusted padding in the main display div for improved visual spacing.
    - Removed the display of days used to streamline the information presented.

--- src/App.tsx
+++ src/App.tsx
@@ -144,13 +144,12 @@ function App() {
 
       {
         data && (
-          <div className="animate-in fade-in space-y-2 text-center">
+          <div className="animate-in fade-in space-y-2 text-center pt-50 pb-10">
             <p className="text-balance font-serif text-7xl font-semibold">
               {data.stats.left > 0
                 ? `${String(data.stats.left)} days left`
                 : 'No days left'}
             </p>
-            <p className="text-sm">Days used: {data.stats.used}</p>
             {data.stats.left <= 0 && (
               <p className="text-red-600 text-lg">You have overstayed!</p>
             )}
